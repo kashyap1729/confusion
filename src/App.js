a77895df3d5bfc92982fe5d4c4104f512dfcacf2
kashyap1729 <kashyap.kiran.1729@gmail.com>
2018-07-09 00:41:07 +0530

    Component1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import {Navbar} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div classname="container">
             <Navbar href="/">Ristorante Con Fusion</Navbar>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
